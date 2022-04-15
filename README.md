766ac413b706bc94a0e19b018fce74156932421c
George Hawkins <george-hawkins@users.noreply.github.com>
2022-04-15 20:04:55 +0200

    Finished off align_images.py.

--- README.md
+++ README.md
@@ -1,22 +1,57 @@
+Image alignment
+===============
+
+Given two images, I wanted to recover the translation, rotation and scale needed to transform one into the other.
+
+| Original | Transformed                      |
+|----------|----------------------------------|
+| ![original](original.png) | ![transformed](transformed.png) |
+
+In my particular situation, I had some video footage that had been stabilized in Blender and I wanted to recover the transformation that had been applied to each frame in such a way that I could replicate the result of stabilization for a particular frame using a _Transform_ node:
+
+![img.png](transform-node.png)
+
+In the end, I managed to do this using the [`estimateAffinePartial2D`](https://docs.opencv.org/4.5.5/d9/d0c/group__calib3d.html#gad767faff73e9cbd8b9d92b955b50062d) function in OpenCV and created the small Python script [`align_images.py`](align_images.py) found here.
+
+Given the original and transformed images, the settings needed for the _Transform_ node can be recovered like this:
+
 ```
-$ python3 -m venv env
-$ source env/bin/activate
-$ pip install --upgrade pip
+$ python align_images.py --original original.png --transform transformed.png 
+Worst match distance: 20.0
+Dimensions: 1920x1080
+
+Transform node values:
+* X: -99.859601
+* Y: 49.416587
+* Angle: -69.988642
+* Scale: 1.399939
 ```
 
-https://pyimagesearch.com/2020/08/31/image-alignment-and-registration-with-opencv/
+Setup
+-----
+
+In order to run `align_image.py`, OpenCV needs to be installed. You can set up a suitable environment as shown here:
 
 ```
-$ pip install opencv-python
-$ pip install imutils
+$ python3 -m venv venv
+$ source venv/bin/activate
+(venv) $ pip install --upgrade pip
+(venv) $ pip install opencv-python
+Installing collected packages: numpy, opencv-python
+Successfully installed numpy-1.22.3 opencv-python-4.5.5.64
 ```
 
----
+As you can see above, installing `opencv-python` also triggers the installation of `numpy` - it does not need to be installed separately.
 
-The turntable backgrounds can be found here <https://coastalpaleo.blogspot.com/2020/06/photogrammetry-turntable-backgrounds.html>
+Now, `align_images.py` can be run as shown up above.
 
-However, oddly - the tweet announcing their availability has larger and higher quality versions of the scale - <https://twitter.com/coastalpaleo/status/1276528937879904258>
+Notes
+-----
 
-The blogspot images are 2048x2041 JPEGs (with all the usual artifacts) while the images on Twitter are 3564x3552 PNGs (without those artifacts).
+The photogrammetry turntable image seen above can be found [here](https://coastalpaleo.blogspot.com/2020/06/photogrammetry-turntable-backgrounds.html) on the Coastal Paleo blog. Oddly though, the versions found on the blog are low-quality JPEG versions of larger high-quality PNGs that can be found with the Tweet [here](https://twitter.com/coastalpaleo/status/1276528937879904258) announcing the blog post.
 
+Links that I found useful:
 
+* MathWorks - [Find Image Rotation and Scale](https://www.mathworks.com/help/images/find-image-rotation-and-scale.html) 
+* PyImageSearch - [Image alignment and registration with OpenCV](https://pyimagesearch.com/2020/08/31/image-alignment-and-registration-with-opencv/). This is very close to what I wanted but recovers a homology matrix rather than the similarity matrix that I wanted.
+* This SO [answer](https://stackoverflow.com/a/54492463/245602) explains why one would use `estimateAffinePartial2D` rather than `estimateAffine2D` (the author of the answer mentions 5-degrees of freedom whereas the `estimateAffinePartial2D` documentation makes clear that it's just 4). This answer also uses `atan2` (rather than `atan` as commonly seen elsewhere) and after some googling, I think this is definitely the right function to be using.
--- align_images.py
+++ align_images.py
@@ -1,146 +1,129 @@
+import argparse
 import math
 import sys
 
 import cv2
-# import imutils
 import numpy as np
 
 
-def decompose_2d_matrix(mat):
-    print(mat)
-    theta0 = np.degrees(np.arctan2(-mat[0, 1], mat[0, 0]))
-    print(theta0)
-    a = mat[0][0]
-    b = mat[0][1]
-    c = mat[0][2]
-    d = mat[1][0]
-    e = mat[1][1]
-    f = mat[1][2]
-
-    delta = a * d - b * c
-
-    translation = [e, f]
-    rotation = 0
-    scale = [0, 0]
-    skew = [0, 0]
-
-    # Apply the QR-like decomposition.
-    if a != 0 or b != 0:
-        r = math.sqrt(a * a + b * b)
-        rotation = math.acos(a / r) if b > 0 else -math.acos(a / r)
-        scale = [r, delta / r]
-        skew = [math.atan((a * c + b * d) / (r * r)), 0]
-    elif c != 0 or d != 0:
-        s = math.sqrt(c * c + d * d)
-        rotation = math.PI / 2 - (math.acos(-c / s) if d > 0 else -math.acos(c / s))
-        scale = [delta / s, s]
-        skew = [0, math.atan((a * c + b * d) / (s * s))]
-    else:
-        # a = b = c = d = 0
-        pass
-
-    print("----")
-    print(translation)
-    # print(rotation)
-    d = math.degrees(rotation)
-    print(d, -d)
-    # print(scale)
-    l = math.sqrt(scale[0] ** 2 + scale[1] ** 2)
-    print(l, (1 / l))
-    # print(skew)
-    print("----")
+_MAX_FEATURES = 500
+_KEEP_PERCENT = 0.2
 
 
-def align_images(image, template, maxFeatures=500, keepPercent=0.2, debug=False):
-    # convert both the input image and template to grayscale
-    imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    templateGray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+def _vertical_flip(height, pts1, pts2):
+    top = height - 1
+    for i in range(len(pts1)):
+        pts1[i, 1] = top - pts1[i, 1]
+        pts2[i, 1] = top - pts2[i, 1]
 
-    # use ORB to detect keypoints and extract (binary) local
-    # invariant features
-    orb = cv2.ORB_create(maxFeatures)
-    (kpsA, descsA) = orb.detectAndCompute(imageGray, None)
-    (kpsB, descsB) = orb.detectAndCompute(templateGray, None)
-    # match the features
-    method = cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING
-    matcher = cv2.DescriptorMatcher_create(method)
-    matches = matcher.match(descsA, descsB, None)
 
-    # sort the matches by their distance (the smaller the distance,
-    # the "more similar" the features are)
-    matches = sorted(matches, key=lambda x:x.distance)
+def _origin_to_center(height, width, pts1, pts2):
+    x_offset = width / 2
+    y_offset = height / 2
+
+    for i in range(len(pts1)):
+        pts1[i, 0] -= x_offset
+        pts1[i, 1] -= y_offset
+        pts2[i, 0] -= x_offset
+        pts2[i, 1] -= y_offset
+
+
+# Adjust the coordinates to match the system used by Blender.
+# TODO: surely it would much cheaper to do these transformations _after_ estimating the similarity matrix.
+def _blender_coordinates(height, width, pts1, pts2):
+    # Flip so top become bottom.
+    _vertical_flip(height, pts1, pts2)
+
+    # Move the origin to the center to the image.
+    _origin_to_center(height, width, pts1, pts2)
+
+
+def align_images(orig_img, transformed_img):
+    # Convert both the original and transformed images grayscale.
+    orig_gray = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
+    transformed_gray = cv2.cvtColor(transformed_img, cv2.COLOR_BGR2GRAY)
+
+    # Use ORB to detect key points and extract (binary) local invariant features.
+    orb = cv2.ORB_create(_MAX_FEATURES)
+    (kp1, des1) = orb.detectAndCompute(orig_gray, None)
+    (kp2, des2) = orb.detectAndCompute(transformed_gray, None)
+
+    # Match the features.
+    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    matches = matcher.match(des1, des2, None)
+
+    # Sort the matches by their distance (the smaller the distance, # the "more similar" the features are).
+    matches = sorted(matches, key=lambda x: x.distance)
     # keep only the top matches
-    keep = int(len(matches) * keepPercent)
+    keep = int(len(matches) * _KEEP_PERCENT)
     matches = matches[:keep]
-    # check to see if we should visualize the matched keypoints
-    # if debug:
-    #     matchedVis = cv2.drawMatches(image, kpsA, template, kpsB,
-    #         matches, None)
-    #     matchedVis = imutils.resize(matchedVis, width=1000)
-    #     cv2.imshow("Matched Keypoints", matchedVis)
-    #     cv2.waitKey(0)
 
-    # allocate memory for the keypoints (x, y)-coordinates from the
-    # top matches -- we'll use these coordinates to compute our
-    # homography matrix
-    ptsA = np.zeros((len(matches), 2), dtype="float")
-    ptsB = np.zeros((len(matches), 2), dtype="float")
-    # loop over the top matches
+    # Print the worst distance that will be used, 30 or above is poor.
+    worst_distance = matches[-1].distance
+    print(f"Worst match distance: {worst_distance}")
+
+    # Extract the best matching points into two arrays of points.
+    pts1 = np.empty((keep, 2), dtype="float")
+    pts2 = np.empty((keep, 2), dtype="float")
     for (i, m) in enumerate(matches):
-        # indicate that the two keypoints in the respective images
-        # map to each other
-        ptsA[i] = kpsA[m.queryIdx].pt
-        ptsB[i] = kpsB[m.trainIdx].pt
+        # Indicate that the two key points in the respective images map to each other.
+        pts1[i] = kp1[m.queryIdx].pt
+        pts2[i] = kp2[m.trainIdx].pt
 
-    # compute the homography matrix between the two sets of matched
-    # points
-    #(H, mask) = cv2.findHomography(ptsA, ptsB, method=cv2.RANSAC)
-    #print(H)
+    height = orig_img.shape[0]
+    width = orig_img.shape[1]
+    print(f"Dimensions: {width}x{height}")
 
-    (M, _) = cv2.estimateAffinePartial2D(ptsA, ptsB)
+    _blender_coordinates(height, width, pts1, pts2)
 
-    print("####")
-    print(template.shape[:2])
-    decompose_2d_matrix(M)
-    print("####")
+    # We want a similarity matrix (with four degrees of freedom) rather than a general affine matrix (with
+    # six degrees of freedom) so that we can decompose it into just scale, rotation and translation. Hence,
+    # the use of `estimateAffinePartial2D` rather than `estimateAffine2D`.
+    (M, _) = cv2.estimateAffinePartial2D(pts1, pts2)
 
-    sys.exit("foobar")
+    t_x = M[0, 2]
+    t_y = M[1, 2]
+    theta = math.atan2(-M[0, 1], M[0, 0])
 
-    # Fake the homography matrix from our 2D matrix
-    # H = np.array([M[0], M[1], [0, 0, 1]], dtype=float)
+    # Is there a better rule for choosing between M[0, 0] and M[1, 0] for calculating scale?
+    # At the moment, I chose the one where the trig result will be furthest from 0.
+    if (math.pi / 4) < abs(theta) < (math.pi * 3 / 4):
+        scale = M[1, 0] / math.sin(theta)
+    else:
+        scale = M[0, 0] / math.cos(theta)
 
-    print(M)
-    #print(H)
-    src = np.array([[[0, 0]]], dtype=float)
-    dst = cv2.transform(src, M)
-    print(dst[0][0])
-    src = np.array([[[1, 0]]], dtype=float)
-    dst = cv2.transform(src, M)
-    xyz2x = dst[0][0][0]
-    xyz2y = dst[0][0][1]
-    print(xyz2x, xyz2y)
-    print(math.sqrt(xyz2x ** 2 + xyz2y ** 2))
-    rotate = math.degrees(math.atan2(xyz2y, xyz2x))
-    print(rotate)
-    print("GCH1")
-    # // extract translation
-    # var p:Point = new Point();
-    # translate = m.transformPoint(p);
-    # m.translate( -translate.x, -translate.y);
-    #
-    # // extract (uniform) scale
-    # p.x = 1.0;
-    # p.y = 0.0;
-    # p = m.transformPoint(p);
-    # scale = p.length;
-    #
-    # // and rotation
-    # rotate = Math.atan2(p.y, p.x);
+    # I can't see where this issue is creeping in but adding 0.5 to x and y gets closer to the desired values.
+    t_x += 0.5
+    t_y += 0.5
 
-    sys.exit("foobar")
+    # Output values to use in Blender Transform node.
+    # Blender views any value smaller than 0.000001 as 0 hence the use of .6f.
+    print()
+    print("Transform node values:")
+    print(f"* X: {t_x:.6f}")
+    print(f"* Y: {t_y:.6f}")
+    print(f"* Angle: {math.degrees(theta):.6f}")
+    print(f"* Scale: {scale:.6f}")
 
-    # use the homography matrix to align the images
-    (h, w) = template.shape[:2]
-    aligned = cv2.warpPerspective(image, H, (w, h))
-    # return the aligned image
-    return aligned
+
+def load(filename):
+    img = cv2.imread(filename)
+    if img is None:
+        sys.exit(f'Error: could not load "{filename}\"')
+    return img
+
+
+# Usage: --original orig-variations/ra-xx.png --transformed variations5/ra-xx.png
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-o", "--original", required=True, help="path to original image")
+    ap.add_argument("-t", "--transformed", required=True, help="path to transformed image")
+    args = vars(ap.parse_args())
+
+    orig_img = load(args["original"])
+    transformed_img = load(args["transformed"])
+    align_images(orig_img, transformed_img)
+
+
+if __name__ == '__main__':
+    main()
